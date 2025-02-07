58428f5a6e2a096186096cb55b827b085d4ca06e
ounxso <dudtj921@naver.com>
2025-02-07 16:12:40 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,178 @@
+const saveBtn = document.getElementById('save');
+const textInput = document.getElementById('text');
+const fileInput = document.getElementById('file');
+const fillBtn = document.getElementById('fill-btn');
+const destroyBtn = document.getElementById('destroy-btn');
+const eraseBtn = document.getElementById('erase-btn');
+
+const colorOptions = Array.from(
+  // forEach 함수 사용해야 하기 때문에 배열 필요하다.
+
+  document.getElementsByClassName('color-option')
+);
+
+const color = document.getElementById('color');
+const lineWidth = document.getElementById('line-width');
+
+const canvas = document.querySelector('canvas');
+// js에서 canvas 불러오기
+
+const ctx = canvas.getContext('2d');
+// context 작성
+// context -> 캔버스에 그림 그릴 때 사용하는 붓
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.lineWidth = lineWidth.value;
+ctx.lineCap = 'round'; // 그리는 선 -> 둥글게
+let isPainting = false;
+let isFilling = false;
+
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting() {
+  isPainting = true;
+}
+
+function cancelPainting() {
+  isPainting = false;
+  ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
+// 모드 바꾸는 일, 한 모드는 캔버스 전체 지우기 & 다른 한 모드는 선 그리기
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = 'Fill';
+    // 사용자가 채우기 모드일 때, 이 버튼을 클릭한다면, 채우기 모드를 멈추고 modeBtn의 텍스트를 Fill로 바꿔줘서 사용자에게 모드가 바뀌었다는 걸 알려준다.
+  } else {
+    isFilling = true;
+    modeBtn.innerText = 'Draw';
+    // 사용자가 버튼을 클릭했을 때, 채우기 모드가 아니라면, 채우기 모드로 바꾸고, 버튼의 텍스트도 바꿔준다.
+  }
+}
+
+function onCanvasClick() {
+  // isFilling일 때, 캔버스를 클릭하면 캔버스 크기의 새로운 사각형을 만들고, 해당 색상으로 채워준다.
+  if (isFilling) {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+
+function onDestroyClick() {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraseClick() {
+  // Stroke Style을 하얀색으로 바꿔주기
+
+  ctx.strokeStyle = 'white';
+  isFilling = false;
+  fillBtn.innerText = 'Fill';
+}
+
+function onFileChange(event) {
+  const file = event.target.files[0];
+  /* 파일 배열인 이유는, html의 input에 multiple 속성을 추가할 수 있기 때문이다.
+  multiple 속성을 추가하면 사용자가 파일을 여러 개 업로드할 수 있다. */
+
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null; // input에 있는 파일 지워주기
+  };
+}
+
+function onDoubleClick(event) {
+  const text = textInput.value;
+
+  if (text !== ' ') {
+    ctx.save(); // ctx의 현재 상태, 색상, 스타일 등 모든 것을 저장한다.
+
+    ctx.lineWidth = 1; // 여기서 수정 가능하다.
+    ctx.font = '70px serif';
+    ctx.fillRect(text, event.offsetX, event.offsetY);
+    ctx.restore(); // 수정 완료하면, ctx.restore 이라고 쓰면 된다.
+  }
+}
+
+function onSaveClick() {
+  const url = canvas.toDataURL();
+  const a = document.createElement('a'); // 링크 생성
+  a.href = url; // 캔버스에서 불러온 이미지, URL로 간다.
+  a.download = 'myDrawing.png';
+  /* a 태그에 download 속성을 더해준다. 
+  download 속성은, 브라우저에게 href에 있는 콘텐츠를 다운로드 하라고 알리는 역할  */
+  a.click();
+}
+
+canvas.addEventListener('dbclick', onDoubleClick);
+// onDoubleClick -> 더블클릭 이벤트는 기본적으로 mousedown, mouseup이 매우 빠르게 일어날 때 발생한다.
+canvas.addEventListener('mousemove', onMove);
+canvas.addEventListener('mousedown', startPainting);
+canvas.addEventListener('mouseup', cancelPainting);
+canvas.addEventListener('mouseleave', cancelPainting);
+
+canvas.addEventListener('click', onCanvasClick);
+// click은 mousedown과 다르다.
+// click -> mousedown, mouseup이 같이 실행된 걸 의미한다.
+
+lineWidth.addEventListener('change', onLineWidthChange);
+
+color.addEventListener('change', onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener('click', onColorClick));
+
+fillBtn.addEventListener('click', onModeClick);
+destroyBtn.addEventListener('click', onDestroyClick);
+eraseBtn.addEventListener('click', onEraseClick);
+fileInput.addEventListener('click', onFileChange);
+saveBtn.addEventListener('click', onSaveClick);
+
+/* 사각형 채우는 함수
+ctx.fillRect(x값, y값, w값, h값);
+*/
+
+/* 사각형 선 그리는 함수
+ctx.rect(x, y, w, h);
+*/
+
+/* 새 경로 시작하기, 이전 경로와 단절
+ctx.beginPath();
+*/
+
+/*
+moveTo(x, y) -> 브러쉬의 좌표를 움직여 준다.
+lineTo(x, y) -> 라인을 그려준다.
+*/
